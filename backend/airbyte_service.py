f0515a238a4e86b257555533414fde6ed7948f11
Guilherme Diaz-Berrio <guilhermediaz-berrio@gmail.com>
2023-08-21 09:45:19 +0100

    fix: changed to s3 destination

--- backend/airbyte_service.py
+++ backend/airbyte_service.py
@@ -65,7 +65,9 @@ def create_psql_source(
     return res
 
 
-def get_google_ads_consent_url(airbyte_auth: AirbyteAuthService, workspace_id: str):
+def get_google_ads_consent_url(
+    airbyte_auth: AirbyteAuthService, workspace_id: str
+) -> str | operations.InitiateOAuthResponse:
     req = shared.InitiateOauthRequest(
         name=shared.OAuthActorNames.GOOGLE_ADS,
         o_auth_input_configuration=shared.OAuthInputConfiguration(),
@@ -88,7 +90,7 @@ def create_google_ads_source(
     secret_id: str,
     customer_id="6597976726",
     manager_customer_id="9831493179",
-):
+) -> operations.CreateSourceResponse:
     req = shared.SourceCreateRequest(
         configuration=shared.SourceGoogleAds(
             credentials=shared.GoogleAdsCredentials(),  # type: ignore
@@ -113,7 +115,7 @@ def create_azure_destination(
     workspace_id: str,
     azure_blob_storage_account_key: str,
     azure_blob_storage_account_name: str,
-):
+) -> operations.CreateDestinationResponse:
     req = shared.DestinationCreateRequest(
         configuration=shared.DestinationAzureBlobStorage(
             azure_blob_storage_account_key=azure_blob_storage_account_key,
@@ -133,11 +135,56 @@ def create_azure_destination(
     return res
 
 
+def create_s3_destination(
+    airbyte_auth: AirbyteAuthService,
+    workspace_id: str,
+    aws_access_key: str,
+    aws_access_secret: str,
+    s3_bucket_name: str,
+    s3_bucket_path: str,
+    s3_bucket_region: shared.DestinationS3S3BucketRegion = shared.DestinationS3S3BucketRegion.EU_WEST_2,
+) -> operations.CreateDestinationResponse:
+    req = shared.DestinationCreateRequest(
+        configuration=shared.DestinationS3(
+            destination_type=shared.DestinationS3S3.S3,
+            access_key_id=aws_access_key,
+            secret_access_key=aws_access_secret,
+            format=shared.DestinationS3FormatParquetColumnarStorage(
+                format_type=shared.DestinationS3FormatParquetColumnarStorageFormatType.PARQUET,
+            ),
+            s3_bucket_name=s3_bucket_name,
+            s3_bucket_path=s3_bucket_path,
+            s3_bucket_region=s3_bucket_region,
+        ),
+        name="S3 Datalake",
+        workspace_id=workspace_id,
+    )
+
+    res = airbyte_auth.s.destinations.create_destination(req)
+
+    return res
+
+
+def get_stream_properties(
+    airbyte_auth: AirbyteAuthService, source_id: str, destination_id: str
+) -> operations.GetStreamPropertiesResponse:
+    # TODO: Fix the return object
+    req = operations.GetStreamPropertiesRequest(
+        destination_id=destination_id,
+        source_id=source_id,
+        ignore_cache=False,
+    )
+
+    res = airbyte_auth.s.streams.get_stream_properties(req)
+
+    return res
+
+
 def create_connection(
     airbyte_auth: AirbyteAuthService,
     source_id: str,
     destination_id: str,
-):
+) -> operations.CreateConnectionResponse:
     req = shared.ConnectionCreateRequest(
         source_id=source_id, destination_id=destination_id, name="test destination"
     )
--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,9 @@ from airbyte_service import (
     get_google_ads_consent_url,
     create_google_ads_source,
     create_azure_destination,
+    create_s3_destination,
     create_connection,
+    get_stream_properties,
 )
 from dotenv import load_dotenv
 import os
@@ -17,6 +19,9 @@ azure_account_name = os.getenv("azure_account_name")
 azure_storage_key = os.getenv("azure_account_storage_key")
 azure_test_destination_id = os.getenv("azure_test_destination_id")
 gads_test_source_id = os.getenv("gads_test_source_id")
+aws_access_key = os.getenv("aws_access_key")
+aws_access_secret = os.getenv("aws_access_secret")
+
 
 app = FastAPI()
 
@@ -37,6 +42,8 @@ async def googleads_oauth():
 
 @app.get("/oauth_callback")
 async def callback(secret_id: str):
+    if testing_workspace_id is None:
+        return {"response": "No workspace_id"}
     airbyte_auth = AirbyteAuthService(airbyte_token=airbyte_key)
     response = create_google_ads_source(
         airbyte_auth=airbyte_auth,
@@ -55,6 +62,22 @@ async def azure_destination():
         azure_blob_storage_account_name=azure_account_name,
         azure_blob_storage_account_key=azure_storage_key,
     )
+
+    return {"response": response}
+
+
+@app.get("/s3_destination")
+async def s3_destination():
+    airbyte_auth = AirbyteAuthService(airbyte_token=airbyte_key)
+    response = create_s3_destination(
+        airbyte_auth=airbyte_auth,
+        workspace_id=testing_workspace_id,
+        aws_access_key=aws_access_key,
+        aws_access_secret=aws_access_secret,
+        s3_bucket_name="widibeodatalake",
+        s3_bucket_path="airbyte",
+    )
+
     return {"response": response}
 
 
@@ -65,6 +88,18 @@ async def create_gads_connection():
         airbyte_auth=airbyte_auth,
         source_id=gads_test_source_id,
         destination_id=azure_test_destination_id,
+    )
+
+    return {"response": response}
+
+
+@app.get("/stream_properties")
+async def stream_properties():
+    airbyte_auth = AirbyteAuthService(airbyte_token=airbyte_key)
+    response = get_stream_properties(
+        airbyte_auth=airbyte_auth,
+        source_id="539bc4c5-dad0-49a2-928b-85025b4c0498",
+        destination_id="967deeca-2fa4-4e87-8711-e116519420f9",
     )
 
     return {"response": response}
